0a33454431a2fb055a36a5bbe9e271874a5c0b9a
Lea Blup <>
2021-12-07 17:26:54 +0000

    No Comments

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -2,10 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName ="Inventory", menuName ="Assets/Inventory")]
 public class Inventory : ScriptableObject
 {
     [SerializeField]
     public int[] numberOfSlots; 
-    //wqeqw
 
 }
