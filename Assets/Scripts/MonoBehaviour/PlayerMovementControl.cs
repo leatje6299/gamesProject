bcbd7eae361c15dd494c2d443d68175634b006f4
samding222@gmail.com <>
2022-01-29 17:59:15 +0000

    TempCharacter(Made a while ago) + Edited Terrain mesh to be low poly

--- Assets/Scripts/MonoBehaviour/PlayerMovementControl.cs
+++ Assets/Scripts/MonoBehaviour/PlayerMovementControl.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 public class PlayerMovementControl : MonoBehaviour
 {
     protected CharacterController characterController;
-    private float speed = 1f;
+    private float speed = 5f;
     public PlayerStatManagement stats;
 
-    private bool skating = true;
+    private bool skating = false;
     private Vector3 SkatingVector = new Vector2(0, 0);
     private float skatingSpeed = 0f;
 
@@ -23,26 +23,30 @@ public class PlayerMovementControl : MonoBehaviour
         float mH = Input.GetAxis("Horizontal"); //side to side
         float mV = Input.GetAxis("Vertical"); //forwards and backwards
         bool shifted = Input.GetButton("Fire3");
-        //Vector3 temp = ((transform.right * mH) + (transform.forward * mV));
-        //characterController.Move(new Vector3(temp.x,-9.81f,temp.z)*Time.deltaTime*speed);
         if (skating == true)
         {
             Vector3 temp = (transform.forward * mV);
             SkatingVector.Normalize();
             SkatingVector *= 10;
             SkatingVector += temp;
-            if(skatingSpeed<5 && mV>0 && !shifted)
+            if (skatingSpeed < 20 && mV > 0 && !shifted)
             {
-                skatingSpeed += 0.01f;
-            } else if (skatingSpeed > 0 && shifted)
+                skatingSpeed += 0.03f;
+            }
+            else if (skatingSpeed > 0 && shifted)
             {
-                skatingSpeed -= 0.05f;
-            } else if (skatingSpeed > 0)
+                skatingSpeed -= 0.1f;
+            }
+            else if (skatingSpeed > 0)
             {
-                skatingSpeed -= 0.01f;
+                skatingSpeed -= 0.03f;
             }
             characterController.Move(SkatingVector.normalized * skatingSpeed * Time.deltaTime);
-            if(skatingSpeed <= 0 && SkatingVector.sqrMagnitude != 0) { SkatingVector = Vector3.zero;}
+            if (skatingSpeed <= 0 && SkatingVector.sqrMagnitude != 0) { SkatingVector = Vector3.zero; }
+        }
+        else {
+            Vector3 temp = ((transform.right * mH) + (transform.forward * mV));
+            characterController.Move(new Vector3(temp.x,-9.81f,temp.z)*Time.deltaTime*speed);
         }
     }
 
