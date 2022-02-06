f05c33e27779810a1af2d57698f35e76b2bc639c
jeffackon@gmail.com <>
2022-02-06 13:34:39 +0000

    I don't know if my journal was uploaded, so I'll try here

---
+++ Assets/Scripts/MonoBehaviour/JournalScript.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JournalScript : MonoBehaviour
+{
+    public static bool OpenJournal = false;
+    public GameObject JournalUI;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if(OpenJournal)
+            {
+                Close();
+            }
+            else
+            {
+                Open();
+            }
+        }
+    }
+
+    void Close()
+    {
+        JournalUI.SetActive(false);
+        Time.timeScale = 1f;
+        OpenJournal = false;
+    }
+
+    void Open()
+    {
+        JournalUI.SetActive(true);
+        Time.timeScale = 1f;
+        OpenJournal = true;
+    }
+}
