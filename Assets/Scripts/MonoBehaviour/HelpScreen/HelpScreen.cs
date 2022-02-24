6c10871c0c3375966b7e61284a4d6ffad62bd630
samding222@gmail.com <>
2022-02-24 14:42:29 +0000

    Fixed Sphere Cast Bug

--- Assets/Scripts/MonoBehaviour/HelpScreen/HelpScreen.cs
+++ Assets/Scripts/MonoBehaviour/HelpScreen/HelpScreen.cs
@@ -8,16 +8,22 @@ public class HelpScreen : MonoBehaviour
     public SphereCast sphereCast;
     [SerializeField]
     private Text KeyBindHelp;
+    private GameObject currentHit;
 
     private void Update()
     {
+        currentHit = sphereCast.currentHitObj;
+        if (currentHit == null) return;
+        
         if(sphereCast.currentHitObj.tag == "Water")
         {
             KeyBindHelp.text = "Press F to fish";
+            return;
         }
         else
         {
             KeyBindHelp.text = "Press shift to ";
+            return;
         }
     }
 }
