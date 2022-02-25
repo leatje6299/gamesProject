ed19068a3853a2d9c29fa0060d6fc3d915569864
jeffackon@gmail.com <>
2022-02-25 12:20:57 +0000

    A few more sounds and a tutorial manager

---
+++ Assets/Scripts/MonoBehaviour/TutorialManager.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class TutorialManager : MonoBehaviour
+{
+    public GameObject[] popUps;
+    private int popUpIndex;
+
+    // Update is called once per frame
+    void Update()
+    {
+        for(int i = 0; i < popUps.Length; i++)
+        {
+            if(i == popUpIndex)
+            {
+                popUps[i].SetActive(true);
+            }
+            else
+            {
+                popUps[i].SetActive(false);
+            }
+        }
+        if(popUpIndex == 0)
+        {
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                popUpIndex += 1;
+            }
+        }
+        else if(popUpIndex == 1)
+        {
+            if(Input.GetKeyDown(KeyCode.LeftShift))
+            {
+                popUpIndex += 1;
+            }
+        }
+        else if (popUpIndex == 2)
+        {
+            if (Input.GetKeyDown(KeyCode.P))
+            {
+                popUpIndex += 1;
+            }
+        }
+    }
+}
