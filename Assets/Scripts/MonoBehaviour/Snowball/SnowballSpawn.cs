08f38b0b8f82387be36595b308cde9ea4e93be5e
samding222@gmail.com <>
2022-03-03 15:18:42 +0000

    SnowballSpawnChanges

--- Assets/Scripts/MonoBehaviour/Snowball/SnowballSpawn.cs
+++ Assets/Scripts/MonoBehaviour/Snowball/SnowballSpawn.cs
@@ -2,17 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//https://answers.unity.com/questions/1657594/how-to-spawn-prefab-anywhere-within-box-collider-2.html
+//part taken from here to spawn inside box collider
+
 public class SnowballSpawn : MonoBehaviour
 {
     //area of spawn 
     private float spawnSizeArea = 10f;
-    [SerializeField]
-    private GameObject snowball;
-    
+    [SerializeField] private BoxCollider bc;
+    [SerializeField] private GameObject snowball;
+
+    Vector3 cubeSize;
+    Vector3 cubeCenter;
+
     // Update is called once per frame
+
+    private void Awake()
+    {
+        Transform cubeTrans = bc.GetComponent<Transform>();
+        cubeCenter = cubeTrans.position;
+
+        // Multiply by scale because it does affect the size of the collider
+        cubeSize.x = cubeTrans.localScale.x * bc.size.x;
+        cubeSize.y = cubeTrans.localScale.y * bc.size.y;
+        cubeSize.z = cubeTrans.localScale.z * bc.size.z;
+
+        print(cubeCenter);
+    }
     void Update()
     {
-        if(Random.Range(1,100) < 7)
+        if(Random.Range(1,1000) < 2)
         {
             SpawnSnowBall();
         }
@@ -20,10 +39,18 @@ public class SnowballSpawn : MonoBehaviour
 
     private void SpawnSnowBall()
     {
-        Vector2 randPos = Random.insideUnitCircle * spawnSizeArea;
-        Vector3 newPos = new Vector3(randPos.x, 0, randPos.y) + transform.position;
-        
+        //print("spawn");
         //change rotation
-        Instantiate(snowball,newPos,Quaternion.identity);
+        Instantiate(snowball, GetRandomPosition(), Quaternion.identity);
+    }
+
+    private Vector3 GetRandomPosition()
+    {
+        // You can also take off half the bounds of the thing you want in the box, so it doesn't extend outside.
+        // Right now, the center of the prefab could be right on the extents of the box
+        Vector3 randomPosition = new Vector3(Random.Range(-cubeSize.x / 2, cubeSize.x / 2), Random.Range(-cubeSize.y / 2, cubeSize.y / 2), Random.Range(-cubeSize.z / 2, cubeSize.z / 2));
+        print(cubeCenter + randomPosition);
+
+        return cubeCenter + randomPosition;
     }
 }
