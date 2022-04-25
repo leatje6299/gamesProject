f2b0a3da26773774846d6f18c8c936db2486b252
leah.fevrier@gmail.com <>
2022-04-25 19:43:40 +0100

    ?test

--- Assets/Samples/Input System/1.1.0-preview.3/Rebinding UI/RebindActionUI.cs
+++ Assets/Samples/Input System/1.1.0-preview.3/Rebinding UI/RebindActionUI.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine.Events;
+using UnityEngine.InputSystem.Utilities;
 using UnityEngine.UI;
 
 ////TODO: localization support
