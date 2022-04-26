0077f27b4970f10e03164d92eb037a5e705571d8
leah.fevrier@gmail.com <>
2022-04-26 22:22:21 +0100

    fix rebind stuff hopefully

--- Assets/Samples/Input System/1.1.0-preview.3/Rebinding UI/RebindSaveLoad.cs
+++ Assets/Samples/Input System/1.1.0-preview.3/Rebinding UI/RebindSaveLoad.cs
@@ -8,11 +8,20 @@ public class RebindSaveLoad : MonoBehaviour
 {
     public InputActionAsset actions;
 
+    private void Start()
+    {
+        if(this.gameObject.activeSelf)
+        {
+            var rebinds = PlayerPrefs.GetString("rebinds");
+            if (!string.IsNullOrEmpty(rebinds))
+                actions.LoadBindingOverridesFromJson(rebinds);
+        }
+    }
     public void OnEnable()
     {
-        var rebinds = PlayerPrefs.GetString("rebinds");
-        if (!string.IsNullOrEmpty(rebinds))
-            actions.LoadBindingOverridesFromJson(rebinds);
+        //var rebinds = PlayerPrefs.GetString("rebinds");
+        //if (!string.IsNullOrEmpty(rebinds))
+        //    actions.LoadBindingOverridesFromJson(rebinds);
     }
 
     public void OnDisable()
